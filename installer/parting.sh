1e12ab0a837a22ce27b7743f27d08f02aa3b8173
Alexander Scott <alexandermgscott@icloud.com>
2025-05-18 18:31:15 +0100

    Added automatic partition module to the installer!

---
+++ installer/parting.sh
@@ -0,0 +1,16 @@
+echo "Sodium OS Installer Automatic Partitioning Module [by Alexander Scott]"
+
+echo "Enter the disk you want to partition (/dev/XXX)"
+read DISK
+echo "Are you SURE you want to partition this disk, remember, all data will be WIPED from this disk!!!!!! (Type 'Yes, I am sure!')"
+read CONFIRMATION
+
+if [ "$CONFIRMATION" == "Yes, I am sure!" ]; then
+	echo "$DISK is being partitioned now"
+	# Creates a GPT partition table on the disk!
+	echo -e "g\nw\n" | fdisk $DISK
+	# Creates a 100MB partition, makes it into an EFI system partition, creates a regular Linux filesystem partition on the rest of the disk
+	echo -e "n\n\n\n+100M\nt\n1\nn\n\n\n\nw\n" | fdisk $DISK
+	mkfs.fat -F 32 $(echo "$(echo $DISK)1")
+	mkfs.ext4 $(echo "$(echo $DISK)2")
+fi
